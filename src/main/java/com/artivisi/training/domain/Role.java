983c6076207869cb3a7d68f6bbc43918bfea7f1f
Endy Muhardin <endy.muhardin@gmail.com>
2013-02-15 10:15:18 +0700

    menggunakan sequence per table (bukan hibernate_sequence yang disharing ke seluruh table)

---
+++ src/main/java/com/artivisi/training/domain/Role.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.artivisi.training.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author endy
+ */
+@Entity @Table(name="t_role")
+@SequenceGenerator(name = "idgen", sequenceName = "t_role_id_seq", allocationSize=1)
+public class Role {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "idgen")
+    private Integer id;
+    private String kode;
+    private String nama;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    
+}
