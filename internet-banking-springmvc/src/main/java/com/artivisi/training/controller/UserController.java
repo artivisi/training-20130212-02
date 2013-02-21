cb6dea2538c756e77886abf4502d3ba5b9b6143f
Endy Muhardin <endy.muhardin@gmail.com>
2013-02-21 13:49:10 +0700

    ambil data dari database menggunakan project core

--- internet-banking-springmvc/src/main/java/com/artivisi/training/controller/UserController.java
+++ internet-banking-springmvc/src/main/java/com/artivisi/training/controller/UserController.java
@@ -4,6 +4,10 @@
  */
 package com.artivisi.training.controller;
 
+import com.artivisi.training.dao.UserDao;
+import com.artivisi.training.domain.User;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +19,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
     
+    @Autowired private UserDao userDao;
+    
     @RequestMapping("/user/list")
     public ModelMap daftarUser(){
         ModelMap mm = new ModelMap();
+        
+        List<User> semuaUser 
+                = userDao.semuaUser(0, userDao.hitungSemuaUser().intValue());
+        
+        mm.addAttribute("daftarUser", semuaUser);
         
         return mm;
     }
