ab4abde8b9c05ca84e89fb2c03bb54ff47cb6548
Endy Muhardin <endy.muhardin@gmail.com>
2013-02-22 15:47:49 +0700

    implement form edit

--- internet-banking-jsf/src/main/java/com/artivisi/training/jsf/UserController.java
+++ internet-banking-jsf/src/main/java/com/artivisi/training/jsf/UserController.java
@@ -5,9 +5,12 @@
 package com.artivisi.training.jsf;
 
 import com.artivisi.training.dao.UserDao;
+import com.artivisi.training.domain.Role;
 import com.artivisi.training.domain.User;
 import java.util.List;
+import javax.faces.model.ListDataModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -15,25 +18,46 @@ import org.springframework.stereotype.Controller;
  * @author endy
  */
 @Controller
+@Scope("session")
 public class UserController {
     @Autowired private UserDao userDao;
     
     private User currentUser;
+    private ListDataModel<User> listDataModelUser;
+
+    public ListDataModel<User> getListDataModelUser() {
+        listDataModelUser = new ListDataModel<User>(getDaftarUser());
+        return listDataModelUser;
+    }
     
     public List<User> getDaftarUser(){
         return userDao.semuaUser(0, userDao.hitungSemuaUser().intValue());
     }
     
     public String tambah(){
+        refreshCurrentUser();
         return "form?faces-redirect=true";
     }
     
     public String edit(){
+        currentUser = listDataModelUser.getRowData();
         return "form?faces-redirect=true";
+    }
+    
+    public String simpan(){
+        userDao.simpan(currentUser);
+        refreshCurrentUser();
+        return "list?faces-redirect=true";
     }
 
     public User getCurrentUser() {
         return currentUser;
+    }
+
+    private void refreshCurrentUser() {
+        currentUser = new User();
+        Role r = new Role();
+        currentUser.setRole(r);
     }
     
     
