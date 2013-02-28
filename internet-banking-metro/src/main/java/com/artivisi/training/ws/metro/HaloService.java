d4eead94faf9ce48fe25f460199eb5d70ea55733
Endy Muhardin <endy.muhardin@gmail.com>
2013-02-28 14:14:21 +0700

    mengakses object Spring dari Web Service

--- internet-banking-metro/src/main/java/com/artivisi/training/ws/metro/HaloService.java
+++ internet-banking-metro/src/main/java/com/artivisi/training/ws/metro/HaloService.java
@@ -4,16 +4,23 @@
  */
 package com.artivisi.training.ws.metro;
 
+import com.artivisi.training.dao.RoleDao;
+import com.artivisi.training.domain.Role;
+import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 /**
  *
  * @author endy
  */
 @WebService(serviceName = "HaloService")
-public class HaloService {
+public class HaloService extends SpringBeanAutowiringSupport{
+    
+    @Autowired private RoleDao roleDao;
 
     /**
      * This is a sample web service operation
@@ -21,5 +28,17 @@ public class HaloService {
     @WebMethod(operationName = "hello")
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
+    }
+    
+    @WebMethod(operationName = "daftarRole")
+    public List<Role> daftarRole() {
+        List<Role> hasil = roleDao.semuaRole();
+        
+        for (Role role : hasil) {
+            role.setDaftarPermission(null);
+            role.setDaftarUser(null);
+        }
+        
+        return hasil;
     }
 }
